0985014860309b31370b1c851259af9a0abc1852
Kartik Awadh Yadav <120031887+Kartikgc9@users.noreply.github.com>
2023-06-26 17:18:48 +0530

    header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center py-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">
-              Copyright &copy; TSS 2023
+              Copyright &copy; KAY 2023
             </p>
           </div>
         </div>
